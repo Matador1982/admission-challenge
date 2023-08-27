8abb61118cf26633c888b553c98843fb4a8716fd
Matador1982 <142356201+Matador1982@users.noreply.github.com>
2023-08-28 00:18:36 +0300

    Update exercise4-fix_server1.sh

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # add a server2 -> for 192.168.60.11 to  /etc/hosts 
-echo "192.168.60.11   server2" | sudo tee -a /etc/hosts > /dev/null 
+# echo "192.168.60.11   server2" | sudo tee -a /etc/hosts > /dev/null 
 
 # set password for vargant user - server1 (pass: 12345678)
 echo -e "12345678\n12345678" | sudo passwd vagrant
 
 # create "config" file into dir : ~/.ssh$ to accept "StrictHostKeyChecking" automaticaly
 config_text=$(cat <<EOF
+Host server2
+ Hostname 192.168.60.11
+ port 22
+ user vagrant
 Host *
     StrictHostKeyChecking no
     UserKnownHostsFile /dev/null
