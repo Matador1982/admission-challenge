bee05f7d9053de64c09380ddd18e83f85a04ccbc
Matador1982 <142356201+Matador1982@users.noreply.github.com>
2023-08-29 00:47:50 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -2,9 +2,9 @@
 #creating the pair of keys (public + private)
 ssh-keygen -t rsa -C "anton"
 
-#Edit config ssh file for server2 authorization Indentify
+#Edit config ssh file for server1 authorization Indentify
 echo "IdentityFile ~/.ssh/anton" | sudo tee -a ~/.ssh/config
 
 #copy the pablic key to server1
 sudo apt install sshpass
-#sshpass -p '12345678' ssh-copy-id -i ~/.ssh/anton server1
+#sshpass -p '12345678' ssh-copy-id -i ~/.ssh/anton server2
