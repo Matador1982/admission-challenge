122a5517cc6fa552e185f8a0b17acb5cfc43e6ce
Matador1982 <142356201+Matador1982@users.noreply.github.com>
2023-08-29 01:17:16 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -3,7 +3,30 @@
 ssh-keygen -t rsa -b 4096 -C "anton" -f ~/.ssh/anton -N ""
 
 #Edit config ssh file for server2 authorization Indentify
-echo "IdentityFile ~/.ssh/anton" | sudo tee -a ~/.ssh/config
+#echo "IdentityFile ~/.ssh/anton" | sudo tee -a ~/.ssh/config
+
+# create "config" file into dir : ~/.ssh$ to accept "StrictHostKeyChecking" automaticaly
+config_text=$(cat <<EOF
+Host server1
+ Hostname 192.168.60.10
+ port 22
+ user vagrant
+Host *
+    StrictHostKeyChecking no
+    UserKnownHostsFile /dev/null
+
+IdentityFile ~/.ssh/anton    
+EOF
+)
+echo "$config_text" > /home/vagrant/.ssh/config
+
+
+
+
+
+
+
+
 
 #copy the pablic key to server2
 sudo apt update
