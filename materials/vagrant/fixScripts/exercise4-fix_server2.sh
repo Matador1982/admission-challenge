624c69a15c0b0db4ea7e2743a597488613c2fa2f
Matador1982 <142356201+Matador1982@users.noreply.github.com>
2023-08-29 00:50:55 +0300

    Update exercise4-fix_server2.sh

--- materials/vagrant/fixScripts/exercise4-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# add a server1 to  /etc/hosts 
+# add a server1 to /etc/hosts 
 echo "192.168.60.10   server1" | sudo tee -a /etc/hosts > /dev/null 
 
 # set password for vargant user - server1 (pass: 12345678)
