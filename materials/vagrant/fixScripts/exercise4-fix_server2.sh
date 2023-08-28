486e113dc67c4b7e055484632e938fc8ba79349d
Matador1982 <142356201+Matador1982@users.noreply.github.com>
2023-08-29 01:23:08 +0300

    Update exercise4-fix_server2.sh

--- materials/vagrant/fixScripts/exercise4-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server2.sh
@@ -5,19 +5,6 @@ echo "192.168.60.10   server1" | sudo tee -a /etc/hosts > /dev/null
 # set password for vargant user - server1 (pass: 12345678)
 echo -e "12345678\n12345678" | sudo passwd vagrant
 
-# create "config" file into dir : ~/.ssh$ to accept "StrictHostKeyChecking" automaticaly
-config_text=$(cat <<EOF
-Host server1
- Hostname 192.168.60.10
- port 22
- user vagrant
-Host *
-    StrictHostKeyChecking no
-    UserKnownHostsFile /dev/null
-EOF
-)
-echo "$config_text" > /home/vagrant/.ssh/config
-
 # Change some configurations into /etc/ssh/sshd_config to make ssh-pass avaiable 
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo service sshd restart
