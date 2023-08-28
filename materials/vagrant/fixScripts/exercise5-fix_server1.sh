233a96c9c20a246ad8974966a8dab00e62be316d
Matador1982 <142356201+Matador1982@users.noreply.github.com>
2023-08-28 23:36:01 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -7,5 +7,4 @@ echo "IdentityFile ~/.ssh/anton" | sudo tee -a ~/.ssh/config
 
 #copy the pablic key to server2
 sudo apt install sshpass
-ssh-copy-id -i -p '12345678' ~/.ssh/anton server1
 sshpass -p '12345678' ssh-copy-id -i ~/.ssh/anton server2
