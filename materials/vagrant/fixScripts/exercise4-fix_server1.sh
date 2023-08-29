701da3e1ef9b96a7d0e0de8a6130257197eeda85
Matador1982 <142356201+Matador1982@users.noreply.github.com>
2023-08-29 22:19:26 +0300

    Update exercise4-fix_server1.sh

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -2,8 +2,11 @@
 # add a server2 to /etc/hosts 
  echo "192.168.60.11   server2" | sudo tee -a /etc/hosts > /dev/null 
 
-# set password for vargant user - server1 (pass: 12345678)
+# set temporary password for vargant user - server1 (pass: 12345678)
  echo -e "12345678\n12345678" | sudo passwd vagrant
+
+#create a random pass ino the file 
+
 
 # Change some configurations into /etc/ssh/sshd_config to make ssh-pass available 
   sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
