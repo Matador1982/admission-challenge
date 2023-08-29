04fc0cbd3af0f21a2503ab1b25bfd771cc217cdc
Matador1982 <142356201+Matador1982@users.noreply.github.com>
2023-08-30 00:07:45 +0300

    Update exercise4-fix_server1.sh

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
+
 # add a server2 to /etc/hosts 
  echo "192.168.60.11   server2" | sudo tee -a /etc/hosts > /dev/null 
 
 # set temporary password for vargant user - server1 (pass: 12345678)
- echo -e "12345678\n12345678" | sudo passwd vagrant
+# echo -e "12345678\n12345678" | sudo passwd vagrant
 
-#create a random pass ino the file 
+#create a random pass ino the file on (password for server2)
+pass_file1="/home/vagrant/.ssh/ssh_pass_for_server1"
+pass_file2="/home/vagrant/.ssh/ssh_pass_for_server2"
+openssl rand -base64 32 > $pass_file1
+openssl rand -base64 32 > $pass_file2
 
+#lock permission
+chmod 400 $pass_file2
+chmod 400 $pass_file1
 
 # Change some configurations into /etc/ssh/sshd_config to make ssh-pass available 
   sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
   sudo service sshd restart
+echo "You can use this command to connect server2 via pass into the file: sshpass -f $pass_file ssh server2"
+
