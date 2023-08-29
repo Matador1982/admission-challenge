f1691273c6a0105e1dad13891e322dc40fd64400
Matador1982 <142356201+Matador1982@users.noreply.github.com>
2023-08-29 22:26:31 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Change some configurations into /etc/ssh/sshd_config to make ssh-pass avaiable 
-# sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-# sudo service sshd restart
+  sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+  sudo service sshd restart
+  
 # set password for vargant user - server1 (pass: 12345678)
-#  echo -e "12345678\n12345678" | sudo passwd vagrant
+   echo -e "12345678\n12345678" | sudo passwd vagrant
 
 # create "config" file into dir : home/vagrant/.ssh$ 
 config_text=$(cat <<EOF
@@ -21,25 +22,13 @@ EOF
 echo "$config_text" > /home/vagrant/.ssh/config
 
 #creating the pair of keys (public + private)
-#ssh-keygen -t rsa -b 4096 -C "server2" -f /home/vagrant/.ssh/anton -N ""
+ ssh-keygen -t rsa -b 4096 -C "server2" -f /home/vagrant/.ssh/anton -N ""
 
 #copy the pablic key to server2
-#sudo apt update
-#sudo apt install sshpass
-#sshpass -p '12345678' ssh-copy-id -i /home/vagrant/.ssh/anton.pub server2
+ sudo apt update
+ sudo apt install sshpass
+ #sshpass -p '12345678' ssh-copy-id -i /home/vagrant/.ssh/anton.pub server2
 
-# create "into dir : ~/.ssh$ to accept "StrictHostKeyChecking" automaticaly
-config_text=$(cat <<EOF
-Host server2
- Hostname 192.168.60.11
- port 22
- user vagrant
-Host *
-    StrictHostKeyChecking no
-    UserKnownHostsFile /dev/null
 
-IdentityFile ~/.ssh/anton    
-EOF
-)
-echo "$config_text" > /home/vagrant/.ssh/config
+
 
