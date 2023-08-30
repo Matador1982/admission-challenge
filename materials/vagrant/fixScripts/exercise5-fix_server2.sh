78bd32b6134581ad241fad858e7d1615b2500b3a
Matador1982 <142356201+Matador1982@users.noreply.github.com>
2023-08-31 02:43:07 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -29,8 +29,9 @@ echo "$config_text" > /home/vagrant/.ssh/config
  sudo apt update
  sudo apt install sshpass
  sshpass -p '12345678' ssh-copy-id -f -i /home/vagrant/.ssh/server2.pub server1
-#Change permission for a vagrant user 
+#Change permission for a vagrant user for server2 and for dir '/home"
  sudo chown vagrant:vagrant server2
+ sudo chown vagrant:vagrant /home
 #Copy pab key from server1 
 pass_file2="/home/vagrant/.ssh/server2.pub"
 
