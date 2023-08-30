a41d7f81e6a0f18610a3e40209a58a02c25353af
Matador1982 <142356201+Matador1982@users.noreply.github.com>
2023-08-31 01:49:43 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -31,3 +31,6 @@ echo "$config_text" > /home/vagrant/.ssh/config
  sshpass -p '12345678' ssh-copy-id -f -i /home/vagrant/.ssh/server2.pub server1
 #Change permission for a vagrant user 
  sudo chown vagrant:vagrant server2
+#Copy pab key from server1 
+pass_file2="/home/vagrant/.ssh/server2.pub"
+
