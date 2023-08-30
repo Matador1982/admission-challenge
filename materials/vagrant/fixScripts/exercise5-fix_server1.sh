115490d037097bd6ec1b9f4dff6b7c1cece1cec4
Matador1982 <142356201+Matador1982@users.noreply.github.com>
2023-08-31 01:07:34 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Change some configurations into /etc/ssh/sshd_config to make ssh-pass avaiable 
   sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-  sudo sed -i 's/PubkeyAuthentication no/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+  # sudo sed -i 's/PubkeyAuthentication no/PubkeyAuthentication yes/' /etc/ssh/sshd_config
   sudo service sshd restart
   
 # set password for vargant user - server1 (pass: 12345678)
@@ -23,13 +23,4 @@ EOF
 echo "$config_text" > /home/vagrant/.ssh/config
 
 #creating the pair of keys (public + private)
-# ssh-keygen -t rsa -b 4096 -C "server1" -f /home/vagrant/.ssh/server1 -N ""
-
-#copy the pablic key to server2
-# sudo apt update
-# sudo apt install sshpass
-# sshpass -p '12345678' ssh-copy-id -i /home/vagrant/.ssh/anton.pub server2
-
-
-
-
+ ssh-keygen -t rsa -b 4096 -C "server1" -f /home/vagrant/.ssh/server1 -N ""
