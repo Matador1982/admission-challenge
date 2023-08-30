a04482e277cc6fd6a6b15429fe84352a59815730
Matador1982 <142356201+Matador1982@users.noreply.github.com>
2023-08-30 21:25:26 +0300

    Update exercise4-fix_server1.sh

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -7,41 +7,41 @@
 echo -e "12345678\n12345678" | sudo passwd vagrant
 
 #create a random pass ino the file on (password for server2)
-pass_file1="/home/vagrant/.ssh/ssh_pass_for_server1"
-pass_file2="/home/vagrant/.ssh/ssh_pass_for_server2"
-openssl rand -base64 32 > $pass_file1
-openssl rand -base64 32 > $pass_file2
+#pass_file1="/home/vagrant/.ssh/ssh_pass_for_server1"
+#pass_file2="/home/vagrant/.ssh/ssh_pass_for_server2"
+#openssl rand -base64 32 > $pass_file1
+#openssl rand -base64 32 > $pass_file2
 
 #lock permission
-chmod 400 $pass_file2
-chmod 400 $pass_file1
+#chmod 400 $pass_file2
+#chmod 400 $pass_file1
 
 # Change some configurations into /etc/ssh/sshd_config to make ssh-pass available 
   sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-  sudo sed -i 's/PubkeyAuthentication yes/PubkeyAuthentication no/' /etc/ssh/sshd_config
+ # sudo sed -i 's/PubkeyAuthentication yes/PubkeyAuthentication no/' /etc/ssh/sshd_config
   sudo service sshd restart
 
-#create script file to change the vagrant user password from server2 
+#creating the script file that changes the vagrant user password from server2 
 
-# Contents of the script to be generated
-script_content="#!/bin/bash
+# Contents of the script to be generated that 
+#script_content="#!/bin/bash
 
 # Read the password from passfile
-password=$(cat $"/home/vagrant/.ssh/ssh_pass_for_server1")
+#password=$(cat $"/home/vagrant/.ssh/ssh_pass_for_server1")
 
 # Change the password for vagrant user on server1
-echo "vagrant:$password" | sudo chpasswd
+#echo "vagrant:$password" | sudo chpasswd
 "
 
 # Path where you want to create the script file
-script_path="/home/vagrant/.ssh/change_pass.sh"
+# script_path="/home/vagrant/.ssh/change_pass.sh"
 
 # Create the script file with the desired content
-echo "$script_content" > "$script_path"
+#echo "$script_content" > "$script_path"
 
 # Make the script file executable
-chmod +x "$script_path"
+# chmod +x "$script_path"
 
 
-echo "You can use this command to connect server2 via pass into the file: sshpass -f $pass_file2 ssh server2"
+# echo "You can use this command to connect server2 via pass into the file: sshpass -f $pass_file2 ssh server2"
 
