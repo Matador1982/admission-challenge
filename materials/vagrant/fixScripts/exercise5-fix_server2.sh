e4f3f0b1ca46efad318471b34da39f93a3511e8e
Matador1982 <142356201+Matador1982@users.noreply.github.com>
2023-08-31 05:59:07 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -17,8 +17,8 @@
 #Change permission for a vagrant user for server2
  sudo chown vagrant:vagrant /home/vagrant/.ssh/server1
 # give to server1 key for ssh-authorization
-sshpass -p '12345678' sftp -o StrictHostKeyChecking=no vagrant@server1 <<< "put -p /home/vagrant/.ssh/server1.pub /home/vagrant/.ssh/"
-sshpass -p '12345678' sftp -o StrictHostKeyChecking=no vagrant@server1 <<< "put -p /home/vagrant/.ssh/server1 /home/vagrant/.ssh/"
+sshpass -p '12345678' sftp -o StrictHostKeyChecking=no vagrant@192.168.60.10  <<< "put -p /home/vagrant/.ssh/server1.pub /home/vagrant/.ssh/"
+sshpass -p '12345678' sftp -o StrictHostKeyChecking=no vagrant@192.168.60.10  <<< "put -p /home/vagrant/.ssh/server1 /home/vagrant/.ssh/"
 #on server2 put the pablic key into authorized_key
 cat /home/vagrant/.ssh/server1.pub >>/home/vagrant/.ssh/authorized_keys
 sudo rm /home/vagrant/.ssh/server1.pub
