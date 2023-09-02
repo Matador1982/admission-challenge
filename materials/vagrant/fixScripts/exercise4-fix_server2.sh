22e49d2af45574095fbb74f2ccf412bdfbe46cca
Matador1982 <142356201+Matador1982@users.noreply.github.com>
2023-09-02 15:41:35 +0300

    Update exercise4-fix_server2.sh

--- materials/vagrant/fixScripts/exercise4-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # add a server1 to /etc/hosts 
 echo "192.168.60.10   server1" | sudo tee -a /etc/hosts > /dev/null 
+#The pass of servers was set to <12345678> in exercise5 , according the provisions on Vagrantfile this script that run before exercise4. 
